ffd11930e8770f54c8da1648a2b382bcfa2279a5
Freezler <r.l.devries@live.nl>
2024-07-27 20:37:20 +0200

    Beschrijving van de wijzigingen

---
+++ .history/next-env.d_20240724213837.ts
@@ -0,0 +1,9 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+AUTH_SECRET = "83O1j2BkPwe5lCw6lc+FiuR+cYVTFc+D1yC9et6HtMU=" 
+	MONGODB_URI = "mongodb+srv://TestUser:TestUser@bootcamp24q2.qdkaxku.mongodb.net/bootcamp24q2?retryWrites=true&w=majority"
+MONGODB_DB = "bootcamp24q2"
+NEXTAUTH_SECRET = "83O1j2BkPwe5lCw6lc+FiuR+cYVTFc+D1yC9et6HtMU="
